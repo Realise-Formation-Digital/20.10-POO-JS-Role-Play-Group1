47bb1f18ca3bf722709f82ea518b0ac182f8e25a
Fabian Corrales <fabian_corrales@windowslive.com>
2021-01-15 11:06:27 +0100

    Update arme.js
    Creation Classe ARME

--- src/arme.js
+++ src/arme.js
@@ -1,1 +1,12 @@
-class Arme {}
+export default class Arme {
+    #pointsForce= Null;
+    #pointsEndurance= Null;
+    #prix= Null;
+  
+    constructor(pointsForce,pointsEndurance,prix) {
+      this.#pointsForce = pointsForce;
+      this.#pointsEndurance = pointsEndurance;
+      this.#prix = prix;
+    }
+  
+  }
